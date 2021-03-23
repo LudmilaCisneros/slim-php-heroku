1fafd7292f94b54b0ac794d03babd66f4cd9a274
Ludmila Cisneros <ludmilacisneros97@hotmail.com>
2021-03-23 18:44:24 -0300

    first commit

--- app/index.php
+++ app/index.php
@@ -3,7 +3,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require_once '../vendor/autoload.php';
-
+#code
 
 
 $app = new \Slim\App([]);
